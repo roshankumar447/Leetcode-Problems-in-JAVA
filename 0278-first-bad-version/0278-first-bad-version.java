71b21f308c67962de857920d8f2cf9ffbac55576
S ROSHAN KUMAR <roshankumar.591978@gmail.com>
2026-09-21 13:36:55 +0530

    Time: 65 ms (5.6%), Space: 42.3 MB (17.67%) - LeetHub

---
+++ 0278-first-bad-version/0278-first-bad-version.java
@@ -0,0 +1,18 @@
+public class Solution extends VersionControl {
+    public int firstBadVersion(int n) {
+        int s = 1;
+        int e = n;
+
+        while (s < e) {
+            int m = s + (e - s) / 2;
+
+            if (isBadVersion(m)) {
+                e = m;
+            } else {
+                s = m + 1;
+            }
+        }
+
+        return s;
+    }
+}
